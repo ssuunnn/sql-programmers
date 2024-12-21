b7ebe789bcccf4417fa42d66ea86b5cd4c992f9c
Sunwoo Nam <108582620+ssuunnn@users.noreply.github.com>
2024-12-22 00:06:50 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,10 @@
+SELECT II.ITEM_ID,
+       II.ITEM_NAME,
+       II.RARITY
+  FROM ITEM_INFO II
+  JOIN ITEM_TREE IT
+    ON II.ITEM_ID = IT.ITEM_ID
+ WHERE PARENT_ITEM_ID IN (SELECT ITEM_ID
+                            FROM ITEM_INFO
+                           WHERE RARITY = 'RARE')
+ ORDER BY 1 DESC;
